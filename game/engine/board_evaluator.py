d157d79230783f5320f6f778f727b524c511651b
Arkit Patel <arkitpatel3697@gmail.com>
2017-10-28 21:55:24 +0530

    Tutorial 48 updates

--- game/engine/board_evaluator.py
+++ game/engine/board_evaluator.py
@@ -3,5 +3,24 @@ class BoardEvaluator:
     def __init__(self):
         pass
 
-    def evaluate(self):
+    def evaluate(self, board, depth):
         pass
+
+
+class StandardBoardEvaluator(BoardEvaluator):
+
+    def __init__(self):
+        BoardEvaluator.__init__(self)
+
+    def evaluate(self, board, depth):
+        return self.score(board, board.get_white_player(), depth) - \
+               self.score(board, board.get_black_player(), depth)
+
+    def score(self, board, player, depth):
+        return self.value(player)
+
+    def value(self, player):
+        piece_value = 0
+        for piece in player.get_active_pieces():
+            piece_value += piece.get_piece_type()
+        return piece_value
