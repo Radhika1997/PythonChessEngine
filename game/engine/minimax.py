02bb5c20023358478bb485dcdfa7ecf2d367ddaf
Arkit Patel <arkitpatel3697@gmail.com>
2017-10-28 17:47:37 +0530

    Tutorial 46 updates

--- game/engine/minimax.py
+++ game/engine/minimax.py
@@ -1,6 +1,6 @@
 from move_strategy import MoveStrategy
 from board_evaluator import BoardEvaluator
-
+from game.move_status import Status
 
 class MiniMax(MoveStrategy):
 
@@ -12,3 +12,33 @@ class MiniMax(MoveStrategy):
 
     def execute(self, board, depth):
         pass
+
+    def min(self, board, depth):
+
+        if depth == 0:
+            return self.board_evaluate.evaluate()
+
+        lowest_value = 10000000000
+        for move in board.get_current_player().get_legal_moves():
+            move_transition = board.get_current_player().make_move(move)
+            if move_transition.get_move_status() == Status.DONE:
+                current_value = self.max(move_transition.get_transition_board(), depth-1)
+                if current_value <= lowest_value:
+                    lowest_value = current_value
+
+        return lowest_value
+
+    def max(self, board, depth):
+
+        if depth == 0:
+            return self.board_evaluate.evaluate()
+
+        highest_value = -10000000000
+        for move in board.get_current_player().get_legal_moves():
+            move_transition = board.get_current_player().make_move(move)
+            if move_transition.get_move_status() == Status.DONE:
+                current_value = self.min(move_transition.get_transition_board(), depth-1)
+                if current_value >= highest_value:
+                    highest_value = current_value
+
+        return highest_value
